bcb339dda71c1fee0918401e54d09a35d5b306c5
renato-scaroni <scaroni@scaroni-N56VJ.(none)>
2013-11-20 12:33:35 -0200

    links criados entre devices

--- classes.py
+++ classes.py
@@ -1,11 +1,13 @@
-class EP3Simulator:
+class EP3Simulator(object):
 	def __init__(self, entities):
 		self.entities = entities
 		
 	def simulate(self, outputFile):
-		print self.entities
+		for k in self.entities.keys():
+			self.entities[k].Loop()
+			self.entities[k].PrintLinks()
 		
-class Agent:
+class Agent(object):
 	def __init__(self, name):
 		pass	
 
@@ -25,16 +27,86 @@ class DNSServer(Agent):
 	def __init__(self, name):
 		print "DNS Server criado ", name
 
-class Entity:
-	sniffer = None
+class Link(object):
+	def __init__(self):
+		self.delay = -1
+		self.speed = -1
+		self.destinationHost = None
+		self.destinationPort = -1
+	def SetSpeed(self, s):
+		self.speed = s
+	def SetDelay (self, d):
+		self.delay = d
+	def SetDestination(self, d, p):
+		self.destinationHost = d
+		self.destinationPort = p
+
+class Entity(object):
+	_sniffer = None
 	def __init__(self, name):
 		pass
-	
+
+	def Loop():
+		pass
+
 class Host(Entity):
+	links = None
+	def __init__(self):
+		pass
 	def __init__(self, name):
+		if(name == ""):
+			return
 		Entity.__init__(self, name)
-	
+		print "Host criado ", name
+		self._name = name
+
+	def Loop(self):
+		print "executando host ", self._name
+
+	def SetLink(self, d, s, dest, destp):
+		newLink = Link()
+
+		newLink.SetSpeed(s)
+		newLink.SetDelay(d)
+		newLink.SetDestination(dest, destp)
+
+		self.links = newLink
+
+	def PrintLinks(self):		
+		if not self.links.destinationPort == -1:
+			print self._name, "-->", str(self.links.destinationHost)+"."+str(self.links.destinationPort)
+		else:
+			print self._name, "-->", self.links.destinationHost
+
 class Router(Entity):
+	def __init__(self):
+		pass
+
 	def __init__(self, name, interfacesCount):
 		Entity.__init__(self, name)
 		self.interfacesCount = interfacesCount
+		self.links = []
+		for i in range (interfacesCount):
+			self.links.append(Link())
+		print "Router criado ", name
+		self._name = name
+
+	def SetLink(self, d, s, dest, destp, port):
+		if port > self.interfacesCount:
+			print "porta inexistente"
+			return
+ 
+		self.links[port].SetSpeed(s)
+		self.links[port].SetDelay(d)
+		self.links[port].SetDestination(dest, destp)
+
+	def PrintLinks(self):
+		for i in range(self.interfacesCount):
+			l = self.links[i]
+			if not l.destinationPort == -1:
+				print self._name+"."+str(i), "-->", str(l.destinationHost) + "."+ str(l.destinationPort)
+			else:
+				print self._name+"."+str(i), "-->", l.destinationHost
+
+	def Loop(self):
+		print "executando router ", self._name
--- main.py
+++ main.py
@@ -14,6 +14,35 @@ def CreateDevice(name, param):
 		numberOfInterfaces = int(param.split(" ")[3].split("]")[0])
 		return Router(name, numberOfInterfaces)
 
+def GetNumFromString(s):
+	i = 0
+	while not s[:-i].isdigit():
+		i += 1
+	return int(s[:-i])
+
+def CreateLink(data, entities):
+	origDevice = data[2][1:].split(".")[0]
+	if len(data[2][1:].split(".")) >1:
+		origPort = data[2][1:].split(".")[1]
+	else:
+		origPort = -1
+	destDevice = data[3][1:].split(".")
+	if len(data[3][1:].split(".")) >1:
+		destPort = data[3][1:].split(".")[1]
+	else:
+		destPort = -1
+
+	if type(entities[origDevice]) == type(Host("")):
+		entities[origDevice].SetLink(GetNumFromString(data[4]), GetNumFromString(data[5]), destDevice[0], int(destPort))
+	else:
+		entities[origDevice].SetLink(GetNumFromString(data[4]), GetNumFromString(data[5]), destDevice[0], int(destPort), int(origPort))
+
+
+	if type(entities[destDevice[0]]) == type(Host("")):
+		entities[destDevice[0]].SetLink(GetNumFromString(data[4]), GetNumFromString(data[5]), (origDevice), int(origPort))
+	else:
+		entities[destDevice[0]].SetLink(GetNumFromString(data[4]), GetNumFromString(data[5]), (origDevice), int(origPort), int(destPort))
+
 def readInput(inputFilename, outputFilename):
 	entities = {}
 	agents = {}
@@ -26,10 +55,11 @@ def readInput(inputFilename, outputFilename):
 			for i in range(2, len(data)): param += " " + data[i]
 			if "$simulator host" in param or "$simulator router" in param:
 				entities[name] = CreateDevice(name, param)
-			else
-				agents[name] = CreateAgent
-
-
+			else:
+				agents[name] = CreateAgent(name, param)
+		if data[0] == "$simulator":
+			if data[1] == "duplex-link":
+				CreateLink(data, entities)
 
 	f.close()
 	simulator = EP3Simulator(entities)
